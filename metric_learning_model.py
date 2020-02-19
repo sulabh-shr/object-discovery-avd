491ac8191d6ad72abda87304be56fc793abda2fa
Sulabh Shrestha <sulabh.shr@gmail.com>
2020-02-19 18:45:25 -0500

    Add embedding model selection parameter. Add option to concatenate triplets before passing to embedding model.

--- metric_learning_model.py
+++ metric_learning_model.py
@@ -4,14 +4,30 @@ from torchvision import models
 
 
 class MetricLearningNet(nn.Module):
-    def __init__(self, pretrained=True):
+    def __init__(self, model='resnet18', pretrained=True):
+
         super(MetricLearningNet, self).__init__()
+        self.model = model
         self.pretrained = pretrained
-        self.embedder = models.resnet50(pretrained=pretrained)
 
-    def forward(self, ref, pos, neg):
-        ref_emb = self.embedder(ref)
-        pos_emb = self.embedder(pos)
-        neg_emb = self.embedder(neg)
+        if model == 'resnet18':
+            self.embedder = models.resnet18(pretrained=self.pretrained)
+        elif model == 'resnet50':
+            self.embedder = models.resnet50(pretrained=self.pretrained)
+        else:
+            raise NotImplementedError(f'Model {self.model} not found!!!')
 
-        return ref_emb, pos_emb, neg_emb
+    def forward(self, ref, pos, neg, concat=True):
+
+        if concat:
+            batch_size = len(ref)
+            triplets = torch.cat((ref, pos, neg), dim=0)
+            emb = self.embedder(triplets)
+
+            return emb[:batch_size], emb[batch_size:2*batch_size], emb[2*batch_size:]
+        else:
+            ref_emb = self.embedder(ref)
+            pos_emb = self.embedder(pos)
+            neg_emb = self.embedder(neg)
+
+            return ref_emb, pos_emb, neg_emb
